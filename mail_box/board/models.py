302a1605394cb47301e70944df173158f54f2ce1
khw7876 <skanahrwi@naver.com>
2022-07-12 10:05:55 +0900

    feat/board관련 models 만들기

--- mail_box/board/models.py
+++ mail_box/board/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    title = models.CharField("제목", max_length=30)
+    content = models.TextField("내용", max_length=120)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class BoardLike(models.Model):
+    author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    board = models.ForeignKey('board.Board', on_delete=models.CASCADE)
+
+
+class BoardComment(models.Model):
+    author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    board = models.ForeignKey('board.Board', on_delete=models.CASCADE)
+    content = models.TextField("내용", max_length=120)
+    
+
+class BoardCommentLike(models.Model):
+    board_comment_author = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    board_comment = models.ForeignKey('board.BoardComment', on_delete=models.CASCADE)

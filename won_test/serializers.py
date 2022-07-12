e5ecc89254565521ab71a3be591a35ab72c65927
heewon Choi <wonbb3313@gmail.com>
2022-07-12 13:40:13 +0900

    feat/기본적인 유저  회원가입, 로그인 구현완료

---
+++ won_test/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User as UserModel
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = "__all__"
+        
+    def create(self, *args, **kwargs):
+        user = super().create(*args, **kwargs)
+        p = user.password
+        user.set_password(p)
+        user.save()
+        return user
+    
+    def update(ser, *args, **kwargs):
+        user = super().create(*args, **kwargs)
+        p = user.password
+        user.set_password(p)
+        user.save()
+        return user

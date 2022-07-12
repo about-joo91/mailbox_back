bd078adf8ca78bb5d6229782fb79aea8ef50d3ce
wonbbnote <wonbbnote@users.noreply.github.com>
2022-07-12 04:45:30 +0000

    :art: Format Python code with psf/black

--- won_test/views.py
+++ won_test/views.py
@@ -7,18 +7,19 @@ from won_test.serializers import UserSignupSerializer
 
 # Create your views here.
 class UserView(APIView):
-    '''
+    """
     회원정보 추가, 수정 및 탈퇴 기능
-    '''
+    """
+
     def post(self, request):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message":"회원가입 성공하였습니다"}, status = status.HTTP_200_OK)
+            return Response({"message": "회원가입 성공하였습니다"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def put(self, request):
-        return Response({"message": "수정이 완료되었습니다!"}, status = status.HTTP_200_OK)
-    
+        return Response({"message": "수정이 완료되었습니다!"}, status=status.HTTP_200_OK)
+
     def delete(self, request):
-        return Response({"message": "탈퇴가 완료되었습니다!"}, status = status.HTTP_200_OK)
+        return Response({"message": "탈퇴가 완료되었습니다!"}, status=status.HTTP_200_OK)

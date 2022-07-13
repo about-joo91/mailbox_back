3eb0d3e15f15db75075f8c4279df682ea7fc7a4e
hajin <f-a-k@naver.com>
2022-07-13 17:32:31 +0900

    refac/model 모델 수정

--- jin/models.py
+++ jin/models.py
@@ -11,7 +11,7 @@ class WorryCategory(models.Model):
 class Letter(models.Model):
     letter_author = models.ForeignKey("user.User", on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey("WorryCategory", on_delete=models.CASCADE)
-    worryboard = models.ForeignKey("worry_board.WorryBoard", on_delete=models.CASCADE)
+    worryboard = models.OneToOneField("worry_board.WorryBoard", on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=30)
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)

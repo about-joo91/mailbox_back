b205f9f7cc8acdf9c814d16fab261d585634709e
haajinkim <haajinkim@users.noreply.github.com>
2022-07-13 00:46:31 +0000

    :art: Format Python code with psf/black

--- jin/migrations/0004_rename_letter_review_letterreview_and_more.py
+++ jin/migrations/0004_rename_letter_review_letterreview_and_more.py
@@ -9,30 +9,32 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('jin', '0003_rename_category_woory_category_and_more'),
+        ("jin", "0003_rename_category_woory_category_and_more"),
     ]
 
     operations = [
         migrations.RenameModel(
-            old_name='Letter_Review',
-            new_name='LetterReview',
+            old_name="Letter_Review",
+            new_name="LetterReview",
         ),
         migrations.RenameModel(
-            old_name='Letter_Review_Like',
-            new_name='LetterReviewLike',
+            old_name="Letter_Review_Like",
+            new_name="LetterReviewLike",
         ),
         migrations.RenameModel(
-            old_name='User_Letter_Target_User',
-            new_name='UserLetterTargetUser',
+            old_name="User_Letter_Target_User",
+            new_name="UserLetterTargetUser",
         ),
         migrations.RenameModel(
-            old_name='Woory_Category',
-            new_name='WooryCategory',
+            old_name="Woory_Category",
+            new_name="WooryCategory",
         ),
         migrations.AddField(
-            model_name='letter',
-            name='create_date',
-            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            model_name="letter",
+            name="create_date",
+            field=models.DateTimeField(
+                auto_now_add=True, default=django.utils.timezone.now
+            ),
             preserve_default=False,
         ),
     ]

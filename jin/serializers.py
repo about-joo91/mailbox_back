077fb1b613f98a2d1c17e819d74088a099bb962e
haajinkim <haajinkim@users.noreply.github.com>
2022-07-13 11:33:50 +0000

    :art: Format Python code with psf/black

--- jin/serializers.py
+++ jin/serializers.py
@@ -1,8 +1,40 @@
 from rest_framework import serializers
 from .models import LetterReview as LetterReviewModel
 from .models import Letter as LetterModel
-from user.models import UserProfile as UserProfileModel
-from worry_board.models import WorryBoard as WorryBoardModel
+from .models import UserLetterTargetUser as UserLetterTargetUserModel
+
+
+class LetterSerilaizer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        new_post = LetterModel.objects.create(**validated_data)
+        new_post.save()
+        target_user = validated_data.pop("letter_author")
+        UserLetterTargetUserModel(letter=new_post, target_user=target_user).save()
+        return new_post
+
+    class Meta:
+        model = LetterModel
+        fields = [
+            "letter_author",
+            "category",
+            "title",
+            "content",
+            "create_date",
+            "worryboard",
+        ]
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    rank_list = serializers.SerializerMethodField()
+
+    def get_rank_list(self, obj):
+        rank_list_get = UserProfileModel.objects.all().order_by("-mongle_grade")[:10]
+        rank_list = [{"user": rank_list.user.username} for rank_list in rank_list_get]
+        return rank_list
+
+    class Meta:
+        model = UserProfileModel
+        fields = ["rank_list"]
 
 
 class MaiapageSerializer(serializers.ModelSerializer):
@@ -11,7 +43,7 @@ class MaiapageSerializer(serializers.ModelSerializer):
     worry_list = serializers.SerializerMethodField()
     rank_list = serializers.SerializerMethodField()
 
-    def get_best_review(self, request):
+    def get_best_review(self, obj):
         best_review_get = LetterReviewModel.objects.all().order_by("-grade")[:3]
         best_review = [
             {
@@ -37,39 +69,6 @@ class MaiapageSerializer(serializers.ModelSerializer):
         ]
         return live_review
 
-    def get_worry_list(self, request):
-        worry_list = []
-        for cate_get in range(1, 7):
-            worry_gets = WorryBoardModel.objects.filter(category=cate_get).order_by(
-                "-create_date"
-            )[:3]
-            for worry_get in worry_gets:
-                cate = {
-                    "worry_id": worry_get.id,
-                    "category": worry_get.category.id,
-                    "content": worry_get.content,
-                }
-                worry_list.append(cate)
-        return worry_list
-
-    def get_rank_list(self, requset):
-
-        rank_list_get = UserProfileModel.objects.all().order_by("-mongle_grade")[:10]
-        rank_list = [{"user": rank_list.user.username} for rank_list in rank_list_get]
-        return rank_list
-
     class Meta:
         model = LetterReviewModel
-        fields = ["rank_list", "worry_list", "best_review", "live_review"]
-
-
-class LetterSerializer(serializers.ModelSerializer):
-    letter_count = serializers.SerializerMethodField()
-
-    def get_letter_count(self, request):
-        count = request.userlettertargetuser_set.all().count()
-        return count
-
-    class Meta:
-        model = LetterModel
-        fields = ["letter_count"]
+        fields = ["best_review", "live_review"]

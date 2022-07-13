43efb1bea680c9a240b587c61a80508982be0002
about-joo91 <about.joo91@gmail.com>
2022-07-13 14:54:33 +0900

    feat/마이페이지 내 편지, 내가 받은 편지 조회 view 추가

--- mail_box/settings.py
+++ mail_box/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
-    "joo",
+    "my_page",
     "board",
     "jin",
     "worry_board",
---
+++ my_page/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("my_letter", views.MyLetterView.as_view()),
+    path("my_recieved_letter",views.MyRecievedLetterView.as_view())
+]

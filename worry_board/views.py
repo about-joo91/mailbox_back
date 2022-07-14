60896bfe32565eaed5017b4fda172b17a171a994
heewon Choi <wonbb3313@gmail.com>
2022-07-14 21:40:56 +0900

    feat/worryboard post 비속어 딥러닝 추가

--- worry_board/views.py
+++ worry_board/views.py
@@ -2,13 +2,24 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from transformers import (
+    AutoTokenizer,
+    BertForSequenceClassification,
+    TextClassificationPipeline,
+)
 
 from worry_board.models import WorryBoard as WorryBoardModel
 from worry_board.serializers import WorryBoardSerializer
 
+model_name = "smilegate-ai/kor_unsmile"
+model = BertForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+pipe = TextClassificationPipeline(
+    model=model, tokenizer=tokenizer, device=-1, top_k=1, function_to_apply="sigmoid"
+)
+
+
 # Create your views here.
-
-
 class WorryBoardView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -26,14 +37,23 @@ class WorryBoardView(APIView):
 
     def post(self, request):
         request.data["author"] = request.user.id
-        create_worry_board_serializer = WorryBoardSerializer(data=request.data)
-        if create_worry_board_serializer.is_valid(raise_exception=True):
-            create_worry_board_serializer.save()
-            return Response({"message": "고민 게시글을 게시하였습니다."}, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                {"message": "게시에 실패했습니다."}, status=status.HTTP_400_BAD_REQUEST
-            )
+        for result in pipe(request.data["content"])[0]:
+            if result["label"] == "clean":
+                create_worry_board_serializer = WorryBoardSerializer(data=request.data)
+                if create_worry_board_serializer.is_valid(raise_exception=True):
+                    create_worry_board_serializer.save()
+                    return Response(
+                        {"message": "고민 게시글을 게시하였습니다."}, status=status.HTTP_200_OK
+                    )
+                else:
+                    return Response(
+                        {"message": "게시에 실패했습니다."}, status=status.HTTP_400_BAD_REQUEST
+                    )
+            else:
+                return Response(
+                    {"message": "부적절한 내용이 담겨있어 게시글을 올릴 수 없습니다"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
     def put(self, request, worry_board_id):
         update_worry_board = WorryBoardModel.objects.get(id=worry_board_id)

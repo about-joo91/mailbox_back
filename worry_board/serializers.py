de986bf521f9c545c29b4692df5a5d103a98c60c
hajin <f-a-k@naver.com>
2022-07-15 13:16:33 +0900

    refac/create_date 수정, settings.py수정

--- worry_board/serializers.py
+++ worry_board/serializers.py
@@ -4,6 +4,16 @@ from worry_board.models import WorryBoard as WorryBoardModel
 
 
 class WorryBoardSerializer(serializers.ModelSerializer):
+    create_date = serializers.SerializerMethodField()
+
+    def get_create_date(self, obj):
+        format_data = "%m-%d %H:%M"
+
+        time = obj.create_date
+        time_data = time.strftime(format_data)
+
+        return time_data
+
     class Meta:
         model = WorryBoardModel
         fields = ["id", "author", "category", "create_date", "content"]

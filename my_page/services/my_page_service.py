29ce4e24fb55dbcc30e5a9e4bee5023d97b52563
about-joo91 <about.joo91@gmail.com>
2022-07-29 17:28:14 +0900

    feat/not_read_letter_view 뼈대 완성

--- my_page/services/my_page_service.py
+++ my_page/services/my_page_service.py
@@ -1,12 +1,10 @@
-from typing import Dict
-
 from django.db.models import Q
 
 from main_page.models import Letter as LetterModel
 from my_page.serializers import LetterSerializer
 
 
-def get_letter_data_by_user(query: Q, letter_num: int) -> Dict:
+def get_letter_data_by_user(query: Q, letter_num: int) -> dict[str, str]:
     return LetterSerializer(
         LetterModel.objects.select_related("letter_author__userprofile")
         .select_related("letter_author__monglegrade")
@@ -15,3 +13,7 @@ def get_letter_data_by_user(query: Q, letter_num: int) -> Dict:
         .select_related("worryboard__category")
         .filter(query)[letter_num]
     ).data
+
+
+def get_not_read_letter_count(query: Q) -> int:
+    return LetterModel.objects.filter(query).count()

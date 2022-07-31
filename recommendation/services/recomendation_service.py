a3ab2ed4ff63992cba324af6981af90d27b1aa6d
heewon Choi <wonbb3313@gmail.com>
2022-07-31 23:37:57 +0900

    feat/first()사용해 인덱스 에러가 아닌 attributeerror로 수정

--- recommendation/services/recomendation_service.py
+++ recommendation/services/recomendation_service.py
@@ -5,7 +5,6 @@ from main_page.models import Letter as LetterModel
 
 
 
-
 def recommend_worryboard_list(cur_user : object):
     """
     추천시스템 service
@@ -13,15 +12,14 @@ def recommend_worryboard_list(cur_user : object):
     try:
         latest_user_letter = LetterModel.objects.filter(letter_author=cur_user).order_by(
             "-create_date"
-        )[:1]
-        worryboard_id_of_letter = latest_user_letter[0].worryboard.id
+        ).first()
+        worryboard_id_of_letter = latest_user_letter.worryboard.id
         recomendation_sys = recommender.recommend_worryboard
         final_worryboard_list = recomendation_sys.recommend_worries(
             worryboard_id_of_letter, cur_user
         )
         return final_worryboard_list
     
-    except IndexError:
-        print("네가 쓴 편지가 없어서 키에러!!")
+    except AttributeError:
         return HttpResponse(status=204)
 

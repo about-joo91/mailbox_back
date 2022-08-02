91e3e6b4335c420cd31704977776dd7259dc6363
about-joo91 <about.joo91@gmail.com>
2022-08-02 18:42:22 +0900

    refac/nginx 설정 변경

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -30,7 +30,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose_prod.yml run --rm --entrypoint "\
+docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \

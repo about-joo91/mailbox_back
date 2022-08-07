c400cc458a8977dbe3807449aaed8cf6237199ad
khw7876 <khw7876@gmail.com>
2022-08-08 03:13:29 +0900

    request_message 제한수 증가

--- worry_board/models.py
+++ worry_board/models.py
@@ -12,7 +12,7 @@ class WorryBoard(models.Model):
 
 class RequestMessage(models.Model):
     author = models.ForeignKey("user.User", on_delete=models.CASCADE)
-    request_message = models.TextField("요청 메세지", max_length=120)
+    request_message = models.TextField("요청 메세지", max_length=180)
     worry_board = models.ForeignKey("worry_board.WorryBoard", on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
     request_status = models.ForeignKey("RequestStatus", on_delete=models.CASCADE, null=True)
@@ -21,3 +21,8 @@ class RequestMessage(models.Model):
 
 class RequestStatus(models.Model):
     status = models.CharField(max_length=20, unique=True)
+
+
+# class DetailWorryMessage(models.Model):
+#     content = models.TextField("상세내용", max_length=500)
+#     request_message = models.ForeignKey("RequestMessage", on_delete=CASCADE)

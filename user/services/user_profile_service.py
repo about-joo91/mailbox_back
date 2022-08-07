d8f077e8441230c8e1097d822a1ed8a5cc3f0fae
about-joo91 <about.joo91@gmail.com>
2022-08-07 14:28:33 +0900

    /refac revert

--- user/services/user_profile_service.py
+++ user/services/user_profile_service.py
@@ -7,8 +7,6 @@ from user.serializers import UserProfileSerializer
 def get_user_profile_data(user_id: int) -> Dict:
     user_profile = (
         UserProfileModel.objects.filter(user_id=user_id)
-        .select_related("user")
-        .select_related("user__monglegrade")
         .select_related("user__monglegrade__mongle_level")
         .prefetch_related("categories")
         .get()
